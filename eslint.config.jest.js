935decdfa94e4d184f3e59d34e69a88ebe532693
uvarovag <aleanches@gmail.com>
2025-01-18 16:41:25 +0300

    fix: remove util import

--- eslint.config.jest.js
+++ eslint.config.jest.js
@@ -1,15 +1,11 @@
 import jestPlugin from 'eslint-plugin-jest'
 import globals from 'globals'
 
-import { transformGlobals } from './utils/transformGlobals.js'
-
 export default [
     {
         files: ['**/*.test.js', '**/*.test.ts', '**/*.test.jsx', '**/*.test.tsx'],
         languageOptions: {
-            globals: {
-                ...transformGlobals(globals.jest),
-            },
+            globals: globals.jest,
         },
         plugins: {
             jest: jestPlugin,
